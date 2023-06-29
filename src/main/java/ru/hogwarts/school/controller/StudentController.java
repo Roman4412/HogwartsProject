8d431cb93f8a4a920e5299b018cc36e75fe70832
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-06-29 06:28:56 +0300

    upload Avatars

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,20 +1,27 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.dto.FacultyDtoOut;
 import ru.hogwarts.school.dto.StudentDtoIn;
 import ru.hogwarts.school.dto.StudentDtoOut;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
     private final StudentService studentService;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping("{id}")
@@ -50,6 +57,15 @@ public class StudentController {
     @DeleteMapping("{id}")
     public StudentDtoOut deleteStudent(@PathVariable Long id) {
         return studentService.deleteStudent(id);
+    }
+
+    @PutMapping(value = "/{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+     public ResponseEntity<String> uploadAvatar(@PathVariable long studentId, @RequestParam MultipartFile avatar) throws IOException {
+        if(avatar.getSize() >= 1024*300) {
+            return ResponseEntity.badRequest().body("File is too big");
+        }
+            avatarService.uploadAvatar(studentId,avatar);
+            return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/ru/hogwarts/school/dto/StudentDtoOut.java
+++ src/main/java/ru/hogwarts/school/dto/StudentDtoOut.java
@@ -1,16 +1,20 @@
 package ru.hogwarts.school.dto;
 
+import ru.hogwarts.school.model.Avatar;
+
 public class StudentDtoOut {
     private long id;
     private String name;
     private int age;
     private FacultyDtoOut faculty;
+    private Avatar avatar;
 
-    public StudentDtoOut(long id, String name, int age, FacultyDtoOut faculty) {
+    public StudentDtoOut(long id, String name, int age, FacultyDtoOut faculty,  Avatar avatar) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.faculty = faculty;
+        this.avatar = avatar;
     }
 
     public StudentDtoOut() {
@@ -48,4 +52,11 @@ public class StudentDtoOut {
         this.faculty = faculty;
     }
 
+    public Avatar getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Avatar avatar) {
+        this.avatar = avatar;
+    }
 }
