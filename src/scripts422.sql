7897c5fd62e79abcc687ec586e9ed34b4ef3310b
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-07-08 03:59:26 +0300

    + constraints + tables persons and cars + JOIN queries

---
+++ src/scripts422.sql
@@ -0,0 +1,25 @@
+CREATE TABLE persons
+(
+    person_id      SERIAL PRIMARY KEY,
+    name           TEXT    NOT NULL,
+    age            INTEGER NOT NULL,
+    driver_license BOOLEAN NOT NULL,
+    car_id         SERIAL
+);
+
+CREATE TABLE cars
+(
+    car_id    SERIAL PRIMARY KEY,
+    brand     TEXT UNIQUE NOT NULL,
+    model     TEXT UNIQUE NOT NULL,
+    price     money       NOT NULL,
+    person_id SERIAL REFERENCES persons (person_id)
+);
+
+SELECT student.name, student.age, faculty.name
+FROM student
+         INNER JOIN faculty ON student.faculty_id = faculty.id;
+
+SELECT student.name, student.age, avatar.file_path
+FROM student
+         LEFT JOIN avatar ON student.id = avatar.student_id;
