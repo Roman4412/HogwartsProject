c5e294c00d8d7ce08d76b506c9ed3d34358e59f6
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-07-11 00:59:54 +0300

    fix

--- src/scripts422.sql
+++ src/scripts422.sql
@@ -1,19 +1,18 @@
+CREATE TABLE cars
+(
+    car_id SERIAL PRIMARY KEY,
+    brand  TEXT UNIQUE NOT NULL,
+    model  TEXT UNIQUE NOT NULL,
+    price  money       NOT NULL
+);
+
 CREATE TABLE persons
 (
     person_id      SERIAL PRIMARY KEY,
     name           TEXT    NOT NULL,
     age            INTEGER NOT NULL,
     driver_license BOOLEAN NOT NULL,
-    car_id         SERIAL
-);
-
-CREATE TABLE cars
-(
-    car_id    SERIAL PRIMARY KEY,
-    brand     TEXT UNIQUE NOT NULL,
-    model     TEXT UNIQUE NOT NULL,
-    price     money       NOT NULL,
-    person_id SERIAL REFERENCES persons (person_id)
+    car_id         SERIAL REFERENCES cars (car_id)
 );
 
 SELECT student.name, student.age, faculty.name
@@ -22,4 +21,4 @@ FROM student
 
 SELECT student.name, student.age, avatar.file_path
 FROM student
-         LEFT JOIN avatar ON student.id = avatar.student_id;
+         INNER JOIN avatar ON student.id = avatar.student_id;
